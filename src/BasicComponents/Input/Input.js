dd142980267e9982b1c315dbfbb1e7446aadcfa4
Terrorbladezz <anirudhsriram1995@gmail.com>
2021-06-16 00:15:14 +0530

    Fixes for mobile and desktop, stateAdditions

--- src/BasicComponents/Input/Input.js
+++ src/BasicComponents/Input/Input.js
@@ -1,10 +1,24 @@
+import React, {useState} from 'react'
 import "./input.scss"
+import correctIcon from "../../Assets/correctIcon/correctIcon.svg"
 const Input = props => {
+    const [state, setstate] = useState({
+        touched : false,
 
+    })
     return (
+        <div className="inputContainer">
         <div className="input">
             <img src={props.icon} alt="" />
-            <input type={props.input.type} placeholder={props.input.placeholder} />
+            <input 
+                type={props.input.type} 
+                placeholder={props.input.placeholder} 
+                value={props.value} 
+                onChange={(e) => props.change({type:props.input.for, value : e.target.value })}
+                />
+            <img src={correctIcon} alt="verified input" style={{display : props.input.valid ? 'block' : 'none'}} />
+        </div>
+        {/* {props.input.valid && <span>{props.input.errorMessage}</span>} */}
         </div>
     )
 }
--- src/components/SignUp/SignUp.js
+++ src/components/SignUp/SignUp.js
@@ -1,9 +1,66 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
 import "./SignUp.scss";
 import Input from '../../BasicComponents/Input/Input';
-
+import nameIcon from "../../Assets/nameIcon/svg/nameIcon.svg"; // name icon
+import emailIcon from "../../Assets/emailIcon/svg/emailIcon.svg"; // name icon
+import passwordIcon from "../../Assets/passwordIcon/svg/passwordIcon.svg"; // name icon
 
 const SignUp = () => {
+    const [formState, setForm] = useState({
+        Name : {value : 'George Costanza', valid : true},
+        Email: {value : 'georgie_boy@yahoo.com', valid : true},
+        Password: {value : 'asdadsasd', valid : true},
+        TermsAndConditions : {value  : false, valid : false},
+        formValid : false
+    })
+
+
+
+    const checkValidity = (type, value) => {
+        console.log(type, value)
+        switch(type){
+            case "Name":
+                return value === "George Costanza" ? true : false;
+                break;
+
+            case "Email":
+                return value === "georgie_boy@yahoo.com" ? true : false;
+                break;
+            case "Password":
+                return value.length > 6 ? true : false;
+                break;
+            case "TermsAndConditions":
+                return value;
+                break;
+            default:
+                return true;
+                break;
+        }
+    }
+
+    const checkFormValidity = () => {
+        debugger
+        for(let item in formState){
+            if (item != "formValid" && !formState[item].valid){
+                return false;
+            }
+        }
+        return true;
+    }
+
+    const handleChange = ({type,value}) => {
+        let currentFormState = {...formState};
+        delete currentFormState[`${type}`];
+        currentFormState[`${type}`] = {value : value, valid: checkValidity(type, value)};
+        console.log(currentFormState[`${type}`])
+        setForm(currentFormState)
+
+    }
+
+
+
+
+
     return (
         <>
             <div className="container">
@@ -18,48 +75,75 @@ const SignUp = () => {
                     </div>
 
                     <div className="form">
+                        <form>
                         <Input
-                            icon="https://marvel-live.freetls.fastly.net/canvas/2021/6/4cdc9e30196b4b209f3e2a36005ee747?quality=95&fake=.svg&width=108"
+                            change={handleChange}
+                            value={formState.Name.value}
+                            icon={nameIcon}
                             input={{
                                 type: 'text',
-                                placeholder: 'Name'
+                                placeholder: 'Name',
+                                for: 'Name',
+                                errorMessage: 'Name already taken',
+                                valid : formState.Name.valid
                             }}
                         />
                         <Input
-                            icon="https://marvel-live.freetls.fastly.net/canvas/2021/6/7ac500eb32d54840a74fcabcb514a1c5?quality=95&fake=.svg&width=108"
+                            change={handleChange}
+                            value={formState.Email.value}
+                            icon={emailIcon}
                             input={{
                                 type: 'email',
-                                placeholder: 'Email'
+                                placeholder: 'Email',
+                                for: 'Email',
+                                errorMessage: 'Email already in use',
+                                valid : formState.Email.valid
+
+                                
                             }}
                         />
 
                         <Input
-                            icon="https://marvel-live.freetls.fastly.net/canvas/2021/6/c93a51eb8bc948698688d24e765003a6?quality=95&fake=.svg&width=108"
+                            change={handleChange}
+                            value={formState.Password.value}
+                            icon={passwordIcon}
                             input={{
                                 type: 'password',
-                                placeholder: 'Password'
+                                placeholder: 'Password',
+                                for: 'Password',
+                                errorMessage: 'Password must be at least 6 characters long.',
+                                valid : formState.Password.valid
+
                             }}
                         />
                         <div className="tc">
 
-                            <input type="checkbox" id="terms" name="tc" value="Terms and conditions" />
-                            <label for="terms">I agree to the <span>Terms</span> and <span>Privacy Policy</span>.
+                            <input checked={formState.TermsAndConditions.value} 
+                            onChange={() => handleChange({type: 'TermsAndConditions', value : !formState.TermsAndConditions.value})} 
+                            type="checkbox" 
+                            id="terms" 
+                            name="tc" 
+                            value="Terms and conditions" />
+                            <label htmlFor="terms">I agree to the <a href="#">Terms</a> and <a href="#">Privacy Policy</a>.
                     </label>
                         </div>
 
-                        <button className="submit">
+                        <button className="submit"  style={{ opacity: formState.formValid ? 1 : 0.5 }} >
                             <div>
                                 SIGN UP
                                 </div>
                             <div>
                                 <img src="https://marvel-live.freetls.fastly.net/canvas/2021/6/a51494c195b84dfca496a3b4d6eb81b8?quality=95&fake=.svg&width=108" />
                             </div> </button>
-
+                            </form>
                     </div>
                 </div>
 
                 <div className="right">
-                        {/* <img src="https://marvel-live.freetls.fastly.net/canvas/2021/6/8813f4b2402c4f8b8a38e737b8946608?quality=95&fake=.svg&width=108" /> */}
+                    <div>
+                    <img src="https://marvel-live.freetls.fastly.net/canvas/2021/6/8813f4b2402c4f8b8a38e737b8946608?quality=95&fake=.svg&width=108" />  
+                    </div>
+
                 </div>
             </div>
         </>
